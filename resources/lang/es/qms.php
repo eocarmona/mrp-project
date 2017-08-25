2661d1a2981bf5485e8e8731ef0309d63fec721f
ecarmona <ecarmona.swaplicado@gmail.com>
2017-08-25 08:37:23 -0500

    Add start view of quality module

--- resources/lang/es/qms.php
+++ resources/lang/es/qms.php
@@ -4,4 +4,13 @@
 
 return [
       'MODULE'  => 'Módulo Calidad',
+
+      'QRY_BY_STATUS' => 'Productos por estatus',
+      'QRY_BY_STATUS_T' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'IN_INSPECTION' => 'En Inspección',
+      'IN_INSPECTION_T' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'CLASSIFICATION' => 'Clasificación',
+      'CLASSIFICATION_T' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'LOTS' => 'Generación de lotes',
+      'LOTS_T' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 ];
--- resources/views/qms/index.blade.php
+++ resources/views/qms/index.blade.php
@@ -6,12 +6,12 @@
 
   <div class="row">
     @include('front.templates.rapidaccess')
-    <?php echo createBlock(asset('images/wms/whss.gif'), "#", trans('wms.QRY_INVENTORY'), "warning3", trans('wms.QRY_INVENTORY_T'));?>
-    <?php echo createBlock(asset('images/wms/movsan.gif'), "#", trans('wms.MOV_WAREHOUSES'), "warning3", trans('wms.MOV_WAREHOUSES_T'));?>
+    <?php echo createBlock(asset('images/wms/box.gif'), "#", trans('qms.QRY_BY_STATUS'), "warning3", trans('wms.QRY_INVENTORY_T'));?>
+    <?php echo createBlock(asset('images/qms/boxtime.png'), "#", trans('qms.IN_INSPECTION'), "warning3", trans('wms.MOV_WAREHOUSES_T'));?>
   </div>
   <div class="row">
-    <?php echo createBlock(asset('images/wms/movs.gif'), "#", trans('wms.DOC_ASSORTMENT'), "warning3", trans('wms.DOC_ASSORTMENT_T'));?>
-    <?php echo createBlock(asset('images/wms/movss.gif'), "#", trans('wms.DOC_RETURNS'), "warning3", trans('wms.DOC_RETURNS_T'));?>
+    <?php echo createBlock(asset('images/qms/inspection.gif'), "#", trans('qms.CLASSIFICATION'), "warning3", trans('wms.DOC_ASSORTMENT_T'));?>
+    <?php echo createBlock(asset('images/qms/lots.gif'), "#", trans('qms.LOTS'), "warning3", trans('wms.DOC_RETURNS_T'));?>
   </div>
   <div class="row">
     <?php echo createBlock(asset('images/wms/barcode.gif'), "#", trans('wms.LBL_GENERATION'), "warning3", trans('wms.LBL_GENERATION_T'));?>
